3eec3c92b34b2fc43888e69133b751b1a296f76e
Rogério Peixoto <rogerio.c.peixoto@accenture.com>
2016-10-09 23:31:40 -0300

    new challenges

---
+++ 30_days_of_code/Day23BinarySearchTreeBFS.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+    static void levelOrder(Node root){
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()){
+
+            Node tree = q.poll();
+
+            System.out.print(tree.data+" ");
+
+            if (tree.left != null){
+                q.offer(tree.left);
+            }
+
+            if (tree.right != null){
+                q.offer(tree.right);
+            }
+        }
+    }
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        levelOrder(root);
+    }
+}
